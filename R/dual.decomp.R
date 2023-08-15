fda2ba15d77c1aad3ef20bde0b021855d9f248fb
Michael Anderson <52722296+and02709@users.noreply.github.com>
2023-08-14 21:36:47 -0500

    removed rspectra code

--- R/dual.decomp.R
+++ R/dual.decomp.R
@@ -16,17 +16,17 @@ dual.decomp <- function(X, L, npc, n, p){
   #   This matrix is nxn instead of pxp
   M <- Rfast::Tcrossprod(Psi, Psi)
   # Decomposition of Dual supervised form
-  Md <- RSpectra::eigs_sym(M, k=npc, which = "LM")
+  Md <- eigen(M)
   if(npc==1){
-    Sigma_Meval <- sqrt(Md$values)
-    U <- Rfast::mat.mult(solve(Sigma_Meval),Rfast::Crossprod(Md$vectors, Psi))
+    Sigma_Meval <- sqrt(Md$values[1])
+    U <- Rfast::mat.mult(solve(Sigma_Meval),Rfast::Crossprod(Md$vectors[1,], Psi))
     #U <- t(t(Psi)%*%Md$vectors%*%t(solve(Sigma_Meval)))
-    return(list(vectors=t(U), values=Md$values))
+    return(list(vectors=t(U), values=Md$values[1]))
   }
   else{
-    Sigma_Meval <- diag(sqrt(Md$values))
+    Sigma_Meval <- diag(sqrt(Md$values[1:npc]))
     #Ut <- t(t(Psi)%*%Uvectors%*%t(solve(Siggy)))
-    U <- Rfast::mat.mult(solve(Sigma_Meval),Rfast::Crossprod(Md$vectors, Psi))
-    return(list(vectors=t(U), values=Md$values))
+    U <- Rfast::mat.mult(solve(Sigma_Meval),Rfast::Crossprod(Md$vectors[1:npc,], Psi))
+    return(list(vectors=t(U), values=Md$values[1:npc]))
   }
 }
